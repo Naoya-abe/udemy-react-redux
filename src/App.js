b9ef3d5afbaa4775db84a65844eddaeb170e942b
阿部直也 <n.abe@gemcook.com>
2019-04-05 09:12:07 +0900

    [update] state, setstate

--- src/App.js
+++ src/App.js
@@ -1,32 +1,31 @@
-import React from 'react'; // jsxを使う際にこのReactは必要
-import PropTypes from 'prop-types';
+import React, {Component} from 'react'; // jsxを使う際にこのReactは必要
 
-const App = () => {
-  const profiles = [
-    {name: 'Taro', age: 10},
-    {name: 'Hanako', age: 20},
-    {name: 'naoya', age: 24},
-  ];
-  return (
-    <div>
-      {profiles.map((profile, index) => {
-        return <User name={profile.name} age={profile.age} key={index} />;
-      })}
-    </div>
-  );
-};
+const App = () => <Counter />;
 
-const User = props => {
-  return (
-    <div>
-      Hi, I am {props.name}, and I'm {props.age} years old!
-    </div>
-  );
-};
+class Counter extends Component {
+  //最初に実行される
+  constructor(props) {
+    super(props);
+    this.state = {count: 0};
+  }
 
-User.propTypes = {
-  name: PropTypes.string,
-  age: PropTypes.number.isRequired,
-};
+  hundlePlusButton = () => {
+    this.setState({count: this.state.count + 1});
+  };
+
+  hundleMinusButton = () => {
+    this.setState({count: this.state.count - 1});
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <div>count: {this.state.count}</div>
+        <button onClick={this.hundlePlusButton}>+1</button>
+        <button onClick={this.hundleMinusButton}>-1</button>
+      </React.Fragment>
+    );
+  }
+}
 
 export default App;
