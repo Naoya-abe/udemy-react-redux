470574c2c50e5d1b98bb48f8945af96306144f4a
阿部直也 <n.abe@gemcook.com>
2019-04-06 12:15:32 +0900

    [update] implement event new component

--- src/components/events_new.js
+++ src/components/events_new.js
@@ -1,26 +1,83 @@
 import React, {Component} from 'react'; // jsxを使う際にこのReactは必要
 import {connect} from 'react-redux';
-// import {Link} from 'react-router-dom';
+import {Field, reduxForm} from 'redux-form';
+import {Link} from 'react-router-dom';
 
-// import {postEvent} from '../actions';
+import {postEvent} from '../actions';
 
 class EventsNew extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+  renderField(field) {
+    const {
+      input,
+      label,
+      type,
+      meta: {touched, error},
+    } = field;
+
     return (
-      <React.Fragment>
-        <div>foo</div>
-      </React.Fragment>
+      <div>
+        <input {...input} placeholder={label} type={type} />
+        {touched && error && <span>{error}</span>}
+      </div>
+    );
+  }
+
+  async onSubmit(values) {
+    await this.props.postEvent(values);
+    this.props.history.push('/');
+  }
+
+  render() {
+    const {handleSubmit} = this.props;
+    return (
+      <form onSubmit={handleSubmit(this.onSubmit)}>
+        <div>
+          <Field
+            label="Title"
+            name="title"
+            type="text"
+            component={this.renderField}
+          />
+        </div>
+        <div>
+          <Field
+            label="Body"
+            name="body"
+            type="text"
+            component={this.renderField}
+          />
+        </div>
+
+        <div>
+          <input type="submit" value="Submit" disabled={false} />
+          <Link to="/">Cancel</Link>
+        </div>
+      </form>
     );
   }
 }
+
+//何も入力されないのを防ぐvalidation
+const validate = values => {
+  const errors = {};
+
+  if (!values.title) errors.title = 'Enter a title, please.';
+  if (!values.body) errors.body = 'Enter a body, please.';
+
+  return errors;
+};
 
 //stateの情報からこのコンポーネントに必要なpropsを取り出してこのコンポーネント内のpropsとしてmappingする機能を持つ関数
 // const mapStateToProps = state => ({events: state.events});
 
 //あるアクションが発生した時にreducerにtypeに応じた状態遷移を実行させるための関数
-// const mapDispatchToProps = {postEvent};
+const mapDispatchToProps = {postEvent};
 
 export default connect(
   null,
-  null
-)(EventsNew);
+  mapDispatchToProps
+)(reduxForm({validate, form: 'eventNewForm'})(EventsNew));
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,5 @@
-import { combineReducers } from 'redux'
-import events from './events'
+import {combineReducers} from 'redux';
+import {reducer as form} from 'redux-form'; //fromという名前にしてimport
+import events from './events';
 
-export default combineReducers({ events})
+export default combineReducers({events, form});
